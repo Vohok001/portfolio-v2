583cf60877a124f0f7aa5ceeabe4fca9f416adef
khushbu vohora <gulnaz328@gmail.com>
2025-04-01 20:42:21 +0100

    updated avatar

--- src/components/ui/SvgAvatar.jsx
+++ src/components/ui/SvgAvatar.jsx
@@ -88,14 +88,14 @@ const SvgAvatar = () => {
           </clipPath>
 
           <linearGradient id="linear-gradient" x1="102.94" y1="154.47" x2="102.94" y2="36.93" gradientUnits="userSpaceOnUse">
-            <stop offset="0" stopColor="#fff5cc" />
-            <stop offset="0.01" stopColor="#faf0c8" />
-            <stop offset="0.19" stopColor="#c2b599" />
-            <stop offset="0.35" stopColor="#998977" />
-            <stop offset="0.47" stopColor="#806f62" />
-            <stop offset="0.54" stopColor="#77655a" />
-            <stop offset="0.6" stopColor="#77655a" />
-            <stop offset="1" stopColor="#77655a" />
+            <stop offset="0" stopColor="#191617" />
+            <stop offset="0.01" stopColor="#281b20" />
+            <stop offset="0.19" stopColor="#191617" />
+            <stop offset="0.35" stopColor="#281b20" />
+            <stop offset="0.47" stopColor="#281b20" />
+            <stop offset="0.54" stopColor="#191617" />
+            <stop offset="0.6" stopColor="#191617" />
+            <stop offset="1" stopColor="#191617" />
           </linearGradient>
         </defs>
 
@@ -135,12 +135,12 @@ const SvgAvatar = () => {
                   transform: `translate(${movements.hairBack.x}px, ${movements.hairBack.y}px)`
                 }}
               />
-              <path className="neck" d="M114.26 143.16v-14a9.22 9.22 0 10-18.43 0v14c-15.27 2.84-24.74 15.08-24.74 27.33H139c0-12.24-9.5-24.49-24.74-27.33z" fill="#ede3d1" />
+              <path className="neck" d="M114.26 143.16v-14a9.22 9.22 0 10-18.43 0v14c-15.27 2.84-24.74 15.08-24.74 27.33H139c0-12.24-9.5-24.49-24.74-27.33z" fill="#CEAFA0" />
               <path className="top" d="M105.61 167c-30.17 0-25.36-40-25.36 15.84h25.35l25-2.14c-.05-55.79 5.17-13.7-24.99-13.7z" fill="#fff" stroke="#404040" strokeWidth=".5" />
-              <path className="shoulder" d="M95.82 142.87c-16 1.84-29.37 19.5-29.37 40h29.37z" fill="#404040" />
-              <path className="shoulder" d="M114.23 142.67c15.76 1.85 29 19.6 29 40.2h-29z" fill="#404040" />
+              <path className="shoulder" d="M95.82 142.87c-16 1.84-29.37 19.5-29.37 40h29.37z" fill="#111827" />
+              <path className="shoulder" d="M114.23 142.67c15.76 1.85 29 19.6 29 40.2h-29z" fill="#111827" />
             </g>
-            <path className="shadow" d="M95.82 122.36h18.41v14.31s-10.5 5.54-18.41 0z" fill="#efceb9" />
+            <path className="shadow" d="M95.82 122.36h18.41v14.31s-10.5 5.54-18.41 0z" fill="#CEAFA0" />
             <g className="head">
               {/* Face, inner face, and all face elements */}
               <g
@@ -151,16 +151,16 @@ const SvgAvatar = () => {
               >
                 {/* Ears - relative to face movement */}
                 <g className="ear-left ear">
-                  <path d="M63.52 105.14A8.21 8.21 0 0072 113.2a8.36 8.36 0 008.51-8.1A8.21 8.21 0 0072 97a8.36 8.36 0 00-8.48 8.14z" fill="#ede3d1" />
-                  <path d="M68.54 104.48a17 17 0 014.14.41c1.07.31 1.94 1 3 1.31a.39.39 0 00.43-.57c-1.15-2.38-5.49-1.86-7.58-1.67a.26.26 0 000 .52z" fill="#b5aa9a" />
+                  <path d="M63.52 105.14A8.21 8.21 0 0072 113.2a8.36 8.36 0 008.51-8.1A8.21 8.21 0 0072 97a8.36 8.36 0 00-8.48 8.14z" fill="#C49E8D" />
+                  <path d="M68.54 104.48a17 17 0 014.14.41c1.07.31 1.94 1 3 1.31a.39.39 0 00.43-.57c-1.15-2.38-5.49-1.86-7.58-1.67a.26.26 0 000 .52z" fill="black" />
                 </g>
                 <g className="ear-right ear">
-                  <path d="M144.37 105.24a8.2 8.2 0 01-8.37 8.06 8.35 8.35 0 01-8.51-8.1 8.21 8.21 0 018.42-8.06 8.35 8.35 0 018.46 8.1z" fill="#ede3d1" />
-                  <path d="M139.6 104c-2.1-.19-6.43-.72-7.59 1.67a.39.39 0 00.44.57c1.07-.26 1.92-1 3-1.31a17.51 17.51 0 014.15-.41.26.26 0 000-.52z" fill="#b5aa9a" />
+                  <path d="M144.37 105.24a8.2 8.2 0 01-8.37 8.06 8.35 8.35 0 01-8.51-8.1 8.21 8.21 0 018.42-8.06 8.35 8.35 0 018.46 8.1z" fill="#C49E8D" />
+                  <path d="M139.6 104c-2.1-.19-6.43-.72-7.59 1.67a.39.39 0 00.44.57c1.07-.26 1.92-1 3-1.31a17.51 17.51 0 014.15-.41.26.26 0 000-.52z" fill="black" />
                 </g>
 
                 {/* Face */}
-                <rect x="73.99" y="48.26" width="61.54" height="80.49" rx="26.08" transform="rotate(180 104.76 88.5)" fill="#ede3d1" />
+                <rect x="73.99" y="48.26" width="61.54" height="80.49" rx="26.08" transform="rotate(180 104.76 88.5)" fill="#CEAFA0" />
 
                 {/* Inner face with all elements */}
                 <g className="inner-face">
@@ -169,27 +169,27 @@ const SvgAvatar = () => {
                     className="eyebrow-right"
                     d="M120.73 79a9 9 0 00-4-1.22 9.8 9.8 0 00-4.19.87"
                     fill="none"
-                    stroke="#b5aa9a"
+                    stroke="black"
                     strokeWidth="1.04"
                   />
                   <path
                     className="eyebrow-left"
                     d="M97.12 79.41a9.53 9.53 0 00-4-1.11 10.58 10.58 0 00-4.2.76"
                     fill="none"
-                    stroke="#b5aa9a"
+                    stroke="black"
                     strokeWidth="1.04"
                   />
 
                   {/* Mouth */}
-                  <path className="mouth" d="M97 107.52s7.06 4.62 14 1.59" fill="none" stroke="#b5aa9a" strokeWidth="1.04" />
-                  <path className="oh" opacity="0" d="M105.56,117.06c4-.14,5-2.89,4.7-5.64s-1.88-6.7-4.84-6.62-4.73,4.36-4.9,6.72S101.57,117.19,105.56,117.06Z" fill="#262528" />
+                  <path className="mouth" d="M97 107.52s7.06 4.62 14 1.59" fill="none" stroke="black" strokeWidth="1.04" />
+                  <path className="oh" opacity="0" d="M105.56,117.06c4-.14,5-2.89,4.7-5.64s-1.88-6.7-4.84-6.62-4.73,4.36-4.9,6.72S101.57,117.19,105.56,117.06Z" fill="black" />
 
                   {/* Eyes */}
                   <g className="eyes">
                     <path
                       className="eye-left eye"
                       d="M89.48 87.37c-.07 2.08 1.25 3.8 2.94 3.85s3.1-1.59 3.16-3.67-1.25-3.8-2.94-3.85-3.1 1.59-3.16 3.67z"
-                      fill="#2b343b"
+                      fill="black"
                       style={{
                         opacity: isBlinking ? 0 : 1
                       }}
@@ -197,7 +197,7 @@ const SvgAvatar = () => {
                     <path
                       className="eye-right eye"
                       d="M113.67 87.37c-.07 2.08 1.25 3.8 2.94 3.85s3.1-1.59 3.16-3.67-1.25-3.8-2.94-3.85-3.1 1.59-3.16 3.67z"
-                      fill="#2b343b"
+                      fill="black"
                       style={{
                         opacity: isBlinking ? 0 : 1
                       }}
@@ -225,21 +225,18 @@ const SvgAvatar = () => {
                   </g>
 
                   {/* Nose */}
-                  <path className="nose" d="M102.39 98.13s3.09 1.55 5.78 0" fill="none" stroke="#e0d5c1" />
-
-                  {/* Glasses */}
-                  <path className="glasses" d="M133.54 81.76c-4.7-1.42-15.29-2.42-19.83-.45-5.82 2.17-3.18 1.57-8.55 1.17-5.36.4-2.74 1-8.55-1.18-7.3-2.55-15.58-.24-22.25.72v2.75c2.46.24 1.26 6.78 3.06 10.32 2.13 7.23 12.69 9.55 18.19 5.49 3.9-2 7.08-10.32 7.21-12.86 0-1.64 4.15-2.57 4.61.24.11 2.53 3.42 10.69 7.28 12.62 5.5 4 16 1.74 18.17-5.49 1.8-3.54 1.69-9.92 2.88-10.32s.74-2.67 0-2.75-1.02-.1-2.22-.26zM97.25 97.49C90.94 104.81 79 101.2 78 92.3c-.7-2.62-1-7.3 1.27-9.12s6.88-1.87 9.23-2c11.14-.26 16.62 5.6 8.75 16.31zm35.12-5.19c-3.71 17.2-27.26 7.42-22.09-7.36 1.87-3.11 9.09-3.84 11.55-3.73 8.07-.04 12.7 1.79 10.54 11.09z" fill="#c6c6c6" opacity=".48" />
+                  <path className="nose" d="M102.39 98.13s3.09 1.55 5.78 0" fill="none" stroke="black" />
 
                   {/* Blush */}
-                  <path className="blush-left eye" d="M89.9 98.17a2.66 2.66 0 01-1.55-.93 3.73 3.73 0 01-.76-3.12 3 3 0 011-1.56 2 2 0 011.4-.42 3 3 0 012.5 2.72.76.76 0 010 .21 3.19 3.19 0 01.11.91 2.1 2.1 0 01-1.77 2.21 2.07 2.07 0 01-.93-.02zM89.34 96v-.05s-.04.05 0 .05z" fill="#efceb9" fillRule="evenodd" />
-                  <path className="blush-right eye" d="M118.93 98.19a2.09 2.09 0 01-1.77-2.19 3.58 3.58 0 01.1-.91v-.21a3 3 0 012.51-2.72 2 2 0 011.4.42 3 3 0 011 1.56 3.73 3.73 0 01-.76 3.12 2.66 2.66 0 01-1.55.93 2.08 2.08 0 01-.93 0zm1.53-2.2v.05c0 .05.05-.04 0-.04z" fill="#efceb9" fillRule="evenodd" />
+                  <path className="blush-left eye" d="M89.9 98.17a2.66 2.66 0 01-1.55-.93 3.73 3.73 0 01-.76-3.12 3 3 0 011-1.56 2 2 0 011.4-.42 3 3 0 012.5 2.72.76.76 0 010 .21 3.19 3.19 0 01.11.91 2.1 2.1 0 01-1.77 2.21 2.07 2.07 0 01-.93-.02zM89.34 96v-.05s-.04.05 0 .05z" fill="#A0654F" fillRule="evenodd" />
+                  <path className="blush-right eye" d="M118.93 98.19a2.09 2.09 0 01-1.77-2.19 3.58 3.58 0 01.1-.91v-.21a3 3 0 012.51-2.72 2 2 0 011.4.42 3 3 0 011 1.56 3.73 3.73 0 01-.76 3.12 2.66 2.66 0 01-1.55.93 2.08 2.08 0 01-.93 0zm1.53-2.2v.05c0 .05.05-.04 0-.04z" fill="#A0654F" fillRule="evenodd" />
                 </g>
 
                 {/* Hair front */}
                 <path
                   className="hair-front"
                   d="M134.1,57.61C129.22,51.79,118,45,115.33,44.84s-13-1.87-20.65,0-16,4.51-18.77,8.26-6.17,18-4.77,24.41c0,0,3-3.09,10.46-5.73h0s.74-6.33,1.45-7.18a32.29,32.29,0,0,0-.1,6.73,59.67,59.67,0,0,1,8.22-2,37,37,0,0,1,.25-8.11,67.11,67.11,0,0,0,.54,8c2-.32,4.18-.59,6.52-.78h0s.18-2.82.61-5.5c0,0,.28,3.33.6,5.42,1.78-.12,3.64-.19,5.62-.21a76.76,76.76,0,0,1,9.11.45c-.05-2.15,0-6.82-.22-7.36s1.07,2.06,1.54,7.52a51.14,51.14,0,0,1,8.84,1.92c.23-2.37.41-5.93-.3-7.88,0,0,2.1,5,1.9,8.42h0c8.36,3,11.06,7.25,11.06,7.25S139,63.43,134.1,57.61Z"
-                  fill="#77655a"
+                  fill="#191617"
                 />
               </g>
             </g>
